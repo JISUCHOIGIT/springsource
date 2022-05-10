ac1df6bd9ed971d3c1419b20ab862d56355a767c
jisu <jisu2298@gamil.com>
2022-05-10 18:06:08 +0900

    spring source

---
+++ lambda/src/lambda/StreamEx8.java
@@ -0,0 +1,16 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamEx8 {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("사과","딸기","배","바나나","수박","참외","바나나");
+		
+		//부분 출력
+		list.stream().skip(2).limit(3).forEach(s->System.out.println(s));
+
+	}
+
+}
